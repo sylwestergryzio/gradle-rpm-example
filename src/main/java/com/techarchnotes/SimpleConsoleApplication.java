bfe02cfbce9d56d7e61059a4f5973370aa3073f4
Sylwester Gryzio <sylwester.gryzio@gmail.com>
2018-03-05 15:08:15 +0100

    Add gradle project

---
+++ src/main/java/com/techarchnotes/SimpleConsoleApplication.java
@@ -0,0 +1,7 @@
+package com.techarchnotes;
+
+public class SimpleConsoleApplication {
+    public static void main(String[] args) {
+        System.out.println("Hello from Simple Console Application! args = [" + args + "]");
+    }
+}
