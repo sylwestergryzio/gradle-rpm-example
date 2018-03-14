ce3bc954a7b3295c2b67a85f0ad3881758cb13ea
Sylwester Gryzio <sylwester.gryzio@gmail.com>
2018-03-14 11:40:25 +0100

    Updating osplugin

--- src/main/java/com/techarchnotes/SimpleConsoleApplication.java
+++ src/main/java/com/techarchnotes/SimpleConsoleApplication.java
@@ -2,6 +2,6 @@ package com.techarchnotes;
 
 public class SimpleConsoleApplication {
     public static void main(String[] args) {
-        System.out.println("Hello from Simple Console Application version 1.0.0-SNAPSHOT! args = [" + args + "]");
+        System.out.println("Hello from Simple Console Application version 1.0.0-SNAPSHOT    ! args = [" + args + "]");
     }
 }
